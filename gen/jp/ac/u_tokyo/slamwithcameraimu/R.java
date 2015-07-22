e4b40972712b3d022ee5b3d7d9ef71ef38ae1a1a
knagara <kukkusonia@gmail.com>
2015-07-22 18:55:00 +0900

    change button color

--- gen/jp/ac/u_tokyo/slamwithcameraimu/R.java
+++ gen/jp/ac/u_tokyo/slamwithcameraimu/R.java
@@ -10,41 +10,46 @@ package jp.ac.u_tokyo.slamwithcameraimu;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050000;
+        public static final int darkgray=0x7f050001;
+        public static final int white=0x7f050002;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button_login=0x7f06001c;
-        public static final int button_start=0x7f06001d;
-        public static final int button_stop=0x7f06001e;
-        public static final int editText1=0x7f060002;
-        public static final int editText10=0x7f06001b;
-        public static final int editText2=0x7f060005;
-        public static final int editText3=0x7f060008;
-        public static final int editText4=0x7f06000b;
-        public static final int editText5=0x7f06000e;
-        public static final int editText6=0x7f060011;
-        public static final int editText7=0x7f060014;
-        public static final int editText9=0x7f060018;
-        public static final int tableRow1=0x7f060000;
-        public static final int tableRow10=0x7f060019;
-        public static final int tableRow2=0x7f060003;
-        public static final int tableRow3=0x7f060006;
-        public static final int tableRow4=0x7f060009;
-        public static final int tableRow5=0x7f06000c;
-        public static final int tableRow6=0x7f06000f;
-        public static final int tableRow7=0x7f060012;
-        public static final int tableRow9=0x7f060016;
-        public static final int textView1=0x7f060001;
-        public static final int textView10=0x7f06001a;
-        public static final int textView2=0x7f060004;
-        public static final int textView3=0x7f060007;
-        public static final int textView4=0x7f06000a;
-        public static final int textView5=0x7f06000d;
-        public static final int textView6=0x7f060010;
-        public static final int textView7=0x7f060013;
-        public static final int textView8=0x7f060015;
-        public static final int textView9=0x7f060017;
+        public static final int button_login=0x7f07001c;
+        public static final int button_start=0x7f07001d;
+        public static final int button_stop=0x7f07001e;
+        public static final int editText1=0x7f070002;
+        public static final int editText10=0x7f07001b;
+        public static final int editText2=0x7f070005;
+        public static final int editText3=0x7f070008;
+        public static final int editText4=0x7f07000b;
+        public static final int editText5=0x7f07000e;
+        public static final int editText6=0x7f070011;
+        public static final int editText7=0x7f070014;
+        public static final int editText9=0x7f070018;
+        public static final int tableRow1=0x7f070000;
+        public static final int tableRow10=0x7f070019;
+        public static final int tableRow2=0x7f070003;
+        public static final int tableRow3=0x7f070006;
+        public static final int tableRow4=0x7f070009;
+        public static final int tableRow5=0x7f07000c;
+        public static final int tableRow6=0x7f07000f;
+        public static final int tableRow7=0x7f070012;
+        public static final int tableRow9=0x7f070016;
+        public static final int textView1=0x7f070001;
+        public static final int textView10=0x7f07001a;
+        public static final int textView2=0x7f070004;
+        public static final int textView3=0x7f070007;
+        public static final int textView4=0x7f07000a;
+        public static final int textView5=0x7f07000d;
+        public static final int textView6=0x7f070010;
+        public static final int textView7=0x7f070013;
+        public static final int textView8=0x7f070015;
+        public static final int textView9=0x7f070017;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -80,10 +85,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
